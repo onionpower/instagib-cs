e2454b95ed42b1e5f75353a60cd9da5282d42dd1
onionpower <chocoonion@gmail.com>
2018-12-07 11:06:17 +0300

    async exception handling in continuation demo

--- instagib/Program.cs
+++ instagib/Program.cs
@@ -7,71 +7,35 @@ using System.Threading.Tasks;
 namespace instagib
 {
     class Program
-    {        
+    {
         static async Task Main(string[] args)
         {
-            const int rightEdge = 10000;
-            const int series = 1000;
-            var expected = Enumerable.Range(0, rightEdge).Aggregate(0d, ((d, i) => d + i));
-            
-            RunSeries("locked", rightEdge, series, expected, (re) =>
+            try
             {
-                var sync = new object();
-                var total = 0d;
-                Parallel.For(0, re, next =>
+                var task = Task.Run(async () =>
                 {
-                    lock (sync)
-                    {
-                        total += next;
-                    }          
-                });
-                return total;
-            });    
-            
-            RunSeries("interlocked", rightEdge, series, expected, (re) =>
-            {
-                var total = 0d;
-                Parallel.For(0, re, next =>
-                {
-                    var current = total;
-                    if (Interlocked.CompareExchange(ref total, current + next, current) != current)
-                    {
-                        var spinner = new SpinWait();
-     
-                        do
+                    var _ = Task
+                        .Run(async () =>
                         {
-                            spinner.SpinOnce();
-                            current = total;
-                        }
-                        while (Interlocked.CompareExchange(ref total, current + next, current) != current);
-                    }
-                });
-                return total;
-            });
-        }
+                            throw new Exception("Here!"); 
+                        })
+                        .ContinueWith(t =>
+                        {
+                            if (t.IsFaulted)
+                            {
+                                Console.WriteLine($"supressed in continuation: {t.Exception.Message}");
+                            }                            
+                        });
 
-        static void RunSeries(string name, int rightEdge, int series, double expected, Func<int, double> action)
-        {
-            long totalTimeInterlocked = 0;
-            for (int i = 0; i < series; i++)
-            {
-                totalTimeInterlocked += Measure(action, rightEdge, expected);
+                });
             }
-            Console.WriteLine($"{name}: ");
-            Console.WriteLine($"{nameof(series)}: {series}");
-            Console.WriteLine($"mean time: {(float)totalTimeInterlocked / series}");
-        }
-        
-        static long Measure(Func<int, double> action, int rightEdge, double expected)
-        {
-            var watch = Stopwatch.StartNew();
-            double result = action(rightEdge);            
-            watch.Stop();
-            if (result != expected)
+            catch (Exception e)
             {
-                throw new ArithmeticException();
+                Console.WriteLine($"supressed in outter catch: {e.Message}");
             }
-            return watch.ElapsedMilliseconds;
-        }
+
+            Console.ReadLine();
+            Console.WriteLine("done");            
+        }  
     }
 }
